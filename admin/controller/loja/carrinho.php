49176792362f5723a9f58582707abcba7de9365f
lindomar1016 <lindomar1016@hotmail.com>
2019-04-08 18:37:11 -0300

    Função de atualizar produtos do carrinho

--- admin/controller/loja/carrinho.php
+++ admin/controller/loja/carrinho.php
@@ -4,13 +4,17 @@ require_once("../../auto_load.php");
 
 $carrinho = (isset($_SESSION['carrinho']))?$_SESSION['carrinho']:Array();//Se ja existir produto no carrinho, tras ele, caso contrario, tras um array vazio
 $p = new Produto;
-$produto = $p->buscarPorID($_POST['id']);//busca o produto pelo id
 
-if(isset($_SESSION['carrinho'][$produto['id']])){//Se ja existir o produto no carrinho, em vez de adiciona-lo, aumentar a quantidade
-	$_SESSION['carrinho'][$produto['id']]['qntd']++;//adicionar o produto na sessao (carrinho)
-}else{
-	$_SESSION['carrinho'][$produto['id']] = $produto;//adicionar o produto na sessao (carrinho)
-	$_SESSION['carrinho'][$produto['id']]['qntd'] = 1; //Inicia quantidade de produtos com 1;
+if(isset($_POST['id'])){
+	$produto = $p->buscarPorID($_POST['id']);//busca o produto pelo id
+	if(isset($_SESSION['carrinho'][$produto['id']])){//Se ja existir o produto no carrinho, em vez de adiciona-lo, aumentar a quantidade
+		$_SESSION['carrinho'][$produto['id']]['qntd']++;//adicionar o produto na sessao (carrinho)
+	}else{
+		$_SESSION['carrinho'][$produto['id']] = $produto;//adicionar o produto na sessao (carrinho)
+		$_SESSION['carrinho'][$produto['id']]['qntd'] = 1; //Inicia quantidade de produtos com 1;
+	}
+}elseif(isset($_POST['qntd_produto'])){
+	$_SESSION['carrinho'][$_POST['id']]['qntd'] = $_POST['qntd_produto']; //Inicia quantidade de produtos com 1;
 }
 
 echo json_encode($_SESSION['carrinho']);
